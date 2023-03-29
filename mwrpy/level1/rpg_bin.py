Fix units and names

mwrpy/level1/rpg_bin.py
@@ -640,7 +640,7 @@ def read_irt(file_name: str) -> dict:
                 "rain": np.ones(header["n"], np.byte) * Fill_Value_Int,
                 "irt": np.ones([header["n"], header["_n_f"]], np.float32) * Fill_Value_Float,
                 "ir_ele": np.ones(header["n"], np.float32) * Fill_Value_Float,
-                "ir_azi": np.ones(header["n"], np.float32) * Fill_Value_Float,
+                "ir_azimuth_angle": np.ones(header["n"], np.float32) * Fill_Value_Float,
             }
             return vrs
 
@@ -683,7 +683,7 @@ def read_irt(file_name: str) -> dict:
                     ang = np.fromfile(file, np.float32, 1)
                 elif code == 671112000:
                     ang = np.fromfile(file, np.int32, 1)
-                data["ir_ele"][sample], data["ir_azi"][sample] = _angle_calc(ang, code)
+                data["ir_ele"][sample], data["ir_azimuth_angle"][sample] = _angle_calc(ang, code)
             file.close()
             return data
 