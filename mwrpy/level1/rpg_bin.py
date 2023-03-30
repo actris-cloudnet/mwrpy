Fix units and names

mwrpy/level1/rpg_bin.py
@@ -407,7 +407,7 @@ def read_bls(file_name: str) -> dict:
                 "rain": np.ones(header["n"] * header["_n_ang"], np.byte) * Fill_Value_Int,
                 "tb": np.ones([header["n"] * header["_n_ang"], header["_n_f"]], np.float32)
                 * Fill_Value_Float,
-                "ele": np.ones(header["n"] * header["_n_ang"], np.float32) * Fill_Value_Float,
+                "elevation_angle": np.ones(header["n"] * header["_n_ang"], np.float32) * Fill_Value_Float,
                 "azimuth_angle": np.ones(header["n"] * header["_n_ang"], np.float32) * Fill_Value_Float,
             }
             return vrs
@@ -440,7 +440,7 @@ def read_bls(file_name: str) -> dict:
                     sample,
                 ] = np.fromfile(file, np.float32, header["_n_f"])
                 ang = np.fromfile(file, np.int32, 1)
-                data["ele"][sample], data["azimuth_angle"][sample] = _angle_calc(ang, code)
+                data["elevation_angle"][sample], data["azimuth_angle"][sample] = _angle_calc(ang, code)
             file.close()
             return data
 
@@ -480,7 +480,7 @@ def read_brt(file_name: str) -> dict:
                 "time": np.ones(header["n"], np.int32) * Fill_Value_Int,
                 "rain": np.ones(header["n"], np.byte) * Fill_Value_Int,
                 "tb": np.ones([header["n"], header["_n_f"]], np.float32) * Fill_Value_Float,
-                "ele": np.ones(header["n"], np.float32) * Fill_Value_Float,
+                "elevation_angle": np.ones(header["n"], np.float32) * Fill_Value_Float,
                 "azimuth_angle": np.ones(header["n"], np.float32) * Fill_Value_Float,
             }
             return vrs
@@ -523,7 +523,7 @@ def read_brt(file_name: str) -> dict:
                     ang = np.fromfile(file, np.float32, 1)
                 elif code == 666000:
                     ang = np.fromfile(file, np.int32, 1)
-                data["ele"][sample], data["azimuth_angle"][sample] = _angle_calc(ang, code)
+                data["elevation_angle"][sample], data["azimuth_angle"][sample] = _angle_calc(ang, code)
             file.close()
             return data
 
@@ -639,7 +639,7 @@ def read_irt(file_name: str) -> dict:
                 "time": np.ones(header["n"], np.int32) * Fill_Value_Int,
                 "rain": np.ones(header["n"], np.byte) * Fill_Value_Int,
                 "irt": np.ones([header["n"], header["_n_f"]], np.float32) * Fill_Value_Float,
-                "ir_ele": np.ones(header["n"], np.float32) * Fill_Value_Float,
+                "ir_elevation_angle": np.ones(header["n"], np.float32) * Fill_Value_Float,
                 "ir_azimuth_angle": np.ones(header["n"], np.float32) * Fill_Value_Float,
             }
             return vrs
@@ -683,7 +683,7 @@ def read_irt(file_name: str) -> dict:
                     ang = np.fromfile(file, np.float32, 1)
                 elif code == 671112000:
                     ang = np.fromfile(file, np.int32, 1)
-                data["ir_ele"][sample], data["ir_azimuth_angle"][sample] = _angle_calc(ang, code)
+                data["ir_elevation_angle"][sample], data["ir_azimuth_angle"][sample] = _angle_calc(ang, code)
             file.close()
             return data
 
@@ -872,7 +872,7 @@ def read_spc(file_name: str) -> dict:
                 "time": np.ones(header["n"], np.int32) * Fill_Value_Int,
                 "rain": np.ones(header["n"], np.byte) * Fill_Value_Int,
                 "tb": np.ones([header["n"], header["_n_f"]], np.float32) * Fill_Value_Float,
-                "ele": np.ones(header["n"], np.float32) * Fill_Value_Float,
+                "elevation_angle": np.ones(header["n"], np.float32) * Fill_Value_Float,
                 "azimuth_angle": np.ones(header["n"], np.float32) * Fill_Value_Float,
             }
             return vrs
@@ -908,7 +908,7 @@ def read_spc(file_name: str) -> dict:
                     ang = np.fromfile(file, np.float32, 1)
                 elif code == 667000:
                     ang = np.fromfile(file, np.int32, 1)
-                data["ele"][sample], data["azimuth_angle"][sample] = _angle_calc(ang, code)
+                data["elevation_angle"][sample], data["azimuth_angle"][sample] = _angle_calc(ang, code)
             file.close()
             return data
 

mwrpy/level1/write_lev1_nc.py
@@ -144,7 +144,7 @@ def prepare_data(
                     rpg_bin.data["ir_beamwidth"] = params["ir_beamwidth"]
                     add_interpol1d(rpg_bin.data, rpg_irt.data["irt"], rpg_irt.data["time"], "irt")
                     add_interpol1d(
-                        rpg_bin.data, rpg_irt.data["ir_ele"], rpg_irt.data["time"], "ir_ele"
+                        rpg_bin.data, rpg_irt.data["ir_elevation_angle"], rpg_irt.data["time"], "ir_elevation_angle"
                     )
                     add_interpol1d(
                         rpg_bin.data, rpg_irt.data["ir_azimuth_anglemuth_angle"], rpg_irt.data["time"], "ir_azimuth_angle"
@@ -314,7 +314,7 @@ def find_lwcl_free(lev1: dict, ix: np.ndarray) -> tuple:
     if len(freq_31) == 1:
         time = lev1["time"][ix]
         tb = np.squeeze(lev1["tb"][ix, freq_31])
-        tb[(lev1["pointing_flag"][ix] == 1) | (lev1["ele"][ix] < 89.0)] = np.nan
+        tb[(lev1["pointing_flag"][ix] == 1) | (lev1["elevation_angle"][ix] < 89.0)] = np.nan
         tb_df = pd.DataFrame({"Tb": tb}, index=pd.to_datetime(time, unit="s"))
         tb_std = tb_df.rolling("2min", center=True, min_periods=10).std()
         tb_mx = tb_std.rolling("20min", center=True, min_periods=100).max()
@@ -324,7 +324,7 @@ def find_lwcl_free(lev1: dict, ix: np.ndarray) -> tuple:
             irt = lev1["irt"][ix, :]
             irt[irt == Fill_Value_Float] = np.nan
             irt = np.nanmean(irt, axis=1)
-            irt[(lev1["pointing_flag"][ix] == 1) | (lev1["ele"][ix] < 89.0)] = np.nan
+            irt[(lev1["pointing_flag"][ix] == 1) | (lev1["elevation_angle"][ix] < 89.0)] = np.nan
             irt_df = pd.DataFrame({"Irt": irt[:]}, index=pd.to_datetime(time, unit="s"))
             irt_mx = irt_df.rolling("20min", center=True, min_periods=100).max()
             index[(irt_mx["Irt"] > 263.15) & (tb_mx["Tb"] > tb_thres)] = 1
@@ -337,7 +337,7 @@ def find_lwcl_free(lev1: dict, ix: np.ndarray) -> tuple:
         df = df.fillna(method="ffill", limit=120)
         index = np.array(df["index"])
         index[(tb_mx["Tb"] < tb_thres) & (index != 1.0)] = 0.0
-        index[(lev1["ele"][ix] < 89.0) & (index != 1.0)] = 2.0
+        index[(lev1["elevation_angle"][ix] < 89.0) & (index != 1.0)] = 2.0
 
     return np.nan_to_num(index, nan=2).astype(int), status
 
@@ -363,7 +363,7 @@ def _add_bls(brt: dict, bls: dict, hkd: dict, params: dict, site: str) -> None:
     brt.data["time"] = brt.data["time"][ind]
     names = [
         "time_bnds",
-        "ele",
+        "elevation_angle",
         "azimuth_angle",
         "rain",
         "tb",
@@ -384,7 +384,7 @@ def _add_bls(brt: dict, bls: dict, hkd: dict, params: dict, site: str) -> None:
 def _add_blb(brt: dict, blb: dict, hkd: dict, params: dict, site: str) -> None:
     """Add BLB boundary-layer scans using a linear time axis"""
 
-    time_add, time_bnds_add, ele_add, azimuth_angle_add, rain_add, tb_add, status_add = (
+    time_add, time_bnds_add, elevation_angle_add, azimuth_angle_add, rain_add, tb_add, status_add = (
         np.empty([0], dtype=np.int32),
         [],
         [],
@@ -469,7 +469,7 @@ def _add_blb(brt: dict, blb: dict, hkd: dict, params: dict, site: str) -> None:
                     * int(isbit(blb.data["rf_mod"][time_ind], 0)),
                 )
             )
-            ele_add = np.concatenate((ele_add, blb.header["_ang"]))
+            elevation_angle_add = np.concatenate((elevation_angle_add, blb.header["_ang"]))
 
             for ang in range(blb.header["_n_ang"]):
                 if len(tb_add) == 0:
@@ -511,7 +511,7 @@ def _add_blb(brt: dict, blb: dict, hkd: dict, params: dict, site: str) -> None:
         brt.data["time"] = brt.data["time"][ind]
         names = [
             "time_bnds",
-            "ele",
+            "elevation_angle",
             "azimuth_angle",
             "rain",
             "tb",

mwrpy/level2/write_lev2_nc.py
@@ -143,7 +143,7 @@ def get_products(site: str, lev1: dict, data_type: str, params: dict) -> dict:
             (lev1["pointing_flag"][:] == 0)
             & np.any(
                 np.abs(
-                    (np.ones((len(lev1["ele"][:]), len(coeff["ele"]))) * coeff["ele"])
+                    (np.ones((len(lev1["elevation_angle"][:]), len(coeff["ele"]))) * coeff["ele"])
                     - np.transpose(
                         np.ones((len(coeff["ele"]), len(lev1["ele"][:]))) * lev1["ele"][:]
                     )
@@ -157,13 +157,13 @@ def get_products(site: str, lev1: dict, data_type: str, params: dict) -> dict:
                 ["No suitable data found for processing for data type: " + data_type]
             )
         coeff["retrieval_elevation_angles"] = str(
-            np.sort(np.unique(ele_retrieval(lev1["ele"][index], coeff)))
+            np.sort(np.unique(ele_retrieval(lev1["elevation_angle"][index], coeff)))
         )
 
         if coeff["ret_type"] < 2:
-            coeff_offset = offset(lev1["ele"][index])
-            coeff_lin = lin(lev1["ele"][index])
-            coeff_quad = quad(lev1["ele"][index])
+            coeff_offset = offset(lev1["elevation_angle"][index])
+            coeff_lin = lin(lev1["elevation_angle"][index])
+            coeff_quad = quad(lev1["elevation_angle"][index])
             tmp_product = (
                 coeff_offset
                 + np.sum(coeff_lin * ret_in[index, :], axis=1)
@@ -173,12 +173,12 @@ def get_products(site: str, lev1: dict, data_type: str, params: dict) -> dict:
         else:
             tmp_product = np.ones(len(index), np.float32) * Fill_Value_Float
             c_w1, c_w2, fac = (
-                weights1(lev1["ele"][index]),
-                weights2(lev1["ele"][index]),
-                factor(lev1["ele"][index]),
+                weights1(lev1["elevation_angle"][index]),
+                weights2(lev1["elevation_angle"][index]),
+                factor(lev1["elevation_angle"][index]),
             )
-            in_sc, in_os = input_scale(lev1["ele"][index]), input_offset(lev1["ele"][index])
-            op_sc, op_os = output_scale(lev1["ele"][index]), output_offset(lev1["ele"][index])
+            in_sc, in_os = input_scale(lev1["elevation_angle"][index]), input_offset(lev1["elevation_angle"][index])
+            op_sc, op_os = output_scale(lev1["elevation_angle"][index]), output_offset(lev1["elevation_angle"][index])
             for ix, iv in enumerate(index):
                 ret_in[iv, 1:] = (ret_in[iv, 1:] - in_os[ix, :]) * in_sc[ix, :]
                 hidden_layer = np.ones(c_w1.shape[2] + 1, np.float32)
@@ -205,7 +205,7 @@ def get_products(site: str, lev1: dict, data_type: str, params: dict) -> dict:
         if data_type == "2P01":
             product, ret = "temperature", "tpt"
         else:
-            product, ret = "water_vapor_vmr", "hpt"
+            product, ret = "absolute_humidity", "hpt"
 
         coeff = get_mvr_coeff(site, ret, lev1["frequency"][:])
         if coeff[0]["ret_type"] < 2:
@@ -229,9 +229,9 @@ def get_products(site: str, lev1: dict, data_type: str, params: dict) -> dict:
             (lev1["pointing_flag"][:] == 0)
             & np.any(
                 np.abs(
-                    (np.ones((len(lev1["ele"][:]), len(coeff["ele"]))) * coeff["ele"])
+                    (np.ones((len(lev1["elevation_angle"][:]), len(coeff["ele"]))) * coeff["ele"])
                     - np.transpose(
-                        np.ones((len(coeff["ele"]), len(lev1["ele"][:]))) * lev1["ele"][:]
+                        np.ones((len(coeff["ele"]), len(lev1["elevation_angle"][:]))) * lev1["elevation_angle"][:]
                     )
                 )
                 < 0.5,
@@ -243,16 +243,16 @@ def get_products(site: str, lev1: dict, data_type: str, params: dict) -> dict:
                 ["No suitable data found for processing for data type: " + data_type]
             )
         coeff["retrieval_elevation_angles"] = str(
-            np.sort(np.unique(ele_retrieval(lev1["ele"][index], coeff)))
+            np.sort(np.unique(ele_retrieval(lev1["elevation_angle"][index], coeff)))
         )
 
         rpg_dat["altitude"] = coeff["height_grid"][:] + params["station_altitude"]
         rpg_dat[product] = ma.masked_all((len(index), coeff["n_height_grid"]))
 
         if coeff["ret_type"] < 2:
-            coeff_offset = offset(lev1["ele"][index])
-            coeff_lin = lin(lev1["ele"][index])
-            coeff_quad = quad(lev1["ele"][index])
+            coeff_offset = offset(lev1["elevation_angle"][index])
+            coeff_lin = lin(lev1["elevation_angle"][index])
+            coeff_quad = quad(lev1["elevation_angle"][index])
 
             for ialt, _ in enumerate(rpg_dat["altitude"]):
                 rpg_dat[product][:, ialt] = (
@@ -263,12 +263,12 @@ def get_products(site: str, lev1: dict, data_type: str, params: dict) -> dict:
 
         else:
             c_w1, c_w2, fac = (
-                weights1(lev1["ele"][index]),
-                weights2(lev1["ele"][index]),
-                factor(lev1["ele"][index]),
+                weights1(lev1["elevation_angle"][index]),
+                weights2(lev1["elevation_angle"][index]),
+                factor(lev1["elevation_angle"][index]),
             )
-            in_sc, in_os = input_scale(lev1["ele"][index]), input_offset(lev1["ele"][index])
-            op_sc, op_os = output_scale(lev1["ele"][index]), output_offset(lev1["ele"][index])
+            in_sc, in_os = input_scale(lev1["elevation_angle"][index]), input_offset(lev1["elevation_angle"][index])
+            op_sc, op_os = output_scale(lev1["elevation_angle"][index]), output_offset(lev1["elevation_angle"][index])
 
             for ix, iv in enumerate(index):
                 hidden_in = np.concatenate(([1.0], (ret_in[iv, 1:] - in_os[ix, :]) * in_sc[ix, :]))
@@ -279,7 +279,7 @@ def get_products(site: str, lev1: dict, data_type: str, params: dict) -> dict:
                     np.tanh(fac[ix] * hidden_layer.dot(c_w2[ix, :, :])) * op_sc[ix, :]
                     + op_os[ix, :]
                 )
-                if product == "water_vapor_vmr":
+                if product == "absolue_humidity":
                     rpg_dat[product][ix, :] = rpg_dat[product][ix, :] / 1000.0
 
     elif data_type == "2P02":
@@ -305,8 +305,8 @@ def get_products(site: str, lev1: dict, data_type: str, params: dict) -> dict:
         )
 
         ix0 = np.where(
-            (lev1["ele"][:] > coeff["ele"][0] - 0.5)
-            & (lev1["ele"][:] < coeff["ele"][0] + 0.5)
+            (lev1["elevation_angle"][:] > coeff["ele"][0] - 0.5)
+            & (lev1["elevation_angle"][:] < coeff["ele"][0] + 0.5)
             & (lev1["pointing_flag"][:] == 1)
             & (np.arange(len(lev1["time"])) + len(coeff["ele"]) < len(lev1["time"]))
         )[0]
@@ -316,7 +316,7 @@ def get_products(site: str, lev1: dict, data_type: str, params: dict) -> dict:
         )
 
         for ix0v in ix0:
-            if (ix0v + len(coeff["ele"]) < len(lev1["time"])) & (np.allclose(lev1["ele"][ix0v : ix0v + len(coeff["ele"])], coeff["ele"], atol=0.5)):
+            if (ix0v + len(coeff["ele"]) < len(lev1["time"])) & (np.allclose(lev1["elevation_angle"][ix0v : ix0v + len(coeff["ele"])], coeff["ele"], atol=0.5)):
                 ibl = np.append(ibl, [np.array(range(ix0v, ix0v + len(coeff["ele"])))], axis=0)
                 tb = np.concatenate(
                     (
@@ -387,7 +387,7 @@ def get_products(site: str, lev1: dict, data_type: str, params: dict) -> dict:
 
         hum_int = interpol_2d(
             hum_dat.variables["time"][:],
-            hum_dat.variables["water_vapor_vmr"][:, :],
+            hum_dat.variables["absolute_humidity"][:, :],
             tem_dat.variables["time"][:],
         )
 
@@ -424,8 +424,8 @@ def _combine_lev1(
         "time_bnds",
         "station_latitude",
         "station_longitude",
-        "azi",
-        "ele",
+        "azimuth_angle",
+        "elevation_angle",
     ]
     if index != []:
         for ivars in lev1_vars:
@@ -486,8 +486,8 @@ def _test_BL_scan(site: str, lev1: dict) -> bool:
     BL_scan = True
     coeff = get_mvr_coeff(site, "tpb", lev1["frequency"][:])
     BL_ind = np.where(
-        (lev1["ele"][:] > coeff[0]["ele"][0] - 0.5)
-        & (lev1["ele"][:] < coeff[0]["ele"][0] + 0.5)
+        (lev1["elevation_angle"][:] > coeff[0]["ele"][0] - 0.5)
+        & (lev1["elevation_angle"][:] < coeff[0]["ele"][0] + 0.5)
         & (lev1["pointing_flag"][:] == 1)
     )[0]
     if len(BL_ind) == 0: