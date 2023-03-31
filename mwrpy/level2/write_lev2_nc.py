fa5565bd755b1c4b550b1f5ec01d9816a6e19563
tobiasmarke <tmarke@meteo.uni-koeln.de>
2023-03-31 15:43:40 +0200

    fix typo

--- mwrpy/level2/write_lev2_nc.py
+++ mwrpy/level2/write_lev2_nc.py
@@ -299,7 +299,7 @@ def get_products(site: str, lev1: netCDF4.Dataset, data_type: str, params: dict)
                     np.tanh(fac[ix] * hidden_layer.dot(c_w2[ix, :, :])) * op_sc[ix, :]
                     + op_os[ix, :]
                 )
-                if product == "absolue_humidity":
+                if product == "absolute_humidity":
                     rpg_dat[product][ix, :] = rpg_dat[product][ix, :] / 1000.0
 
     elif data_type == "2P02":
