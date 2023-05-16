9e9fecb5c1ac0b67729e0a3ce7080294abdb2d5d
Simo Tukiainen <simo.tukiainen@fmi.fi>
2023-05-16 13:36:42 +0300

    Run test for all coefficients

--- tests/test_write_lev1_nc.py
+++ tests/test_write_lev1_nc.py
@@ -5,12 +5,14 @@ from mwrpy import lev1_to_nc
 
 PACKAGE_DIR = os.path.abspath(os.path.dirname(__file__))
 DATA_DIR = f"{PACKAGE_DIR}/data"
+COEFFICIENTS_DIR = f"{PACKAGE_DIR}/../mwrpy/site_config/"
 DATE = "2023-04-06"
 
 
 def test_lev1_to_nc():
-    hatpro = lev1_to_nc("hyytiala", "1C01", DATA_DIR)
-    assert hatpro.date == DATE
-    for t in hatpro.data["time"][:]:
-        date = str(datetime.datetime.utcfromtimestamp(t).date())
-        assert date == DATE
+    for site in next(os.walk(COEFFICIENTS_DIR))[1]:
+        hatpro = lev1_to_nc(site, "1C01", DATA_DIR)
+        assert hatpro.date == DATE
+        for t in hatpro.data["time"][:]:
+            date = str(datetime.datetime.utcfromtimestamp(t).date())
+            assert date == DATE
