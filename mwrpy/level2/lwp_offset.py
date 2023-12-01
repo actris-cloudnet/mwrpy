2a42b511f4ddd8b55f2c16493a5ddad63b3708bd
tobiasmarke <tmarke@meteo.uni-koeln.de>
2023-12-01 14:32:37 +0100

    Exclude scans in LWP offset correction

--- mwrpy/level2/lwp_offset.py
+++ mwrpy/level2/lwp_offset.py
@@ -24,20 +24,16 @@ def correct_lwp_offset(
 
     lwcl_i = lev1["liquid_cloud_flag"][index]
     ind = utils.time_to_datetime_index(lev1["time"][index])
-    lwp_df = pd.DataFrame({"Lwp": lwp_org}, index=ind)
+    lwp = np.copy(lwp_org)
+    lwp[elevation_angle[index] < 89.0] = np.nan
+    lwp_df = pd.DataFrame({"Lwp": lwp}, index=ind)
     lwp_std = lwp_df.rolling(
         pd.tseries.frequencies.to_offset("2min"), center=True, min_periods=10
     ).std()
     lwp_max = lwp_std.rolling(
         pd.tseries.frequencies.to_offset("20min"), center=True, min_periods=100
     ).max()
-    lwp = np.copy(lwp_org)
-    lwp[
-        (lwcl_i != 0)
-        | (lwp > 0.06)
-        | (elevation_angle[index] < 89.0)
-        | (lwp_max["Lwp"][:] > 0.003)
-    ] = np.nan
+    lwp[(lwcl_i != 0) | (lwp > 0.06) | (lwp_max["Lwp"][:] > 0.003)] = np.nan
     lwp_df = pd.DataFrame({"Lwp": lwp}, index=ind)
     lwp_offset = lwp_df.rolling(
         pd.tseries.frequencies.to_offset("20min"), center=True, min_periods=100
